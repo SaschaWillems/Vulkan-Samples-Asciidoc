30d321837a9e0016ba4a9edafea87f530935a319
Sascha Willems <webmaster@saschawillems.de>
2023-06-04 12:22:24 +0200

    Conversion script

--- convert.py
+++ convert.py
@@ -37,29 +37,34 @@ for root, dirs, files in os.walk(dir_path):
             markdown_files.append(os.path.join(root, file))
         # print(root, dirs, files)
 
+# Note: Nav needs a lot of manual changes, so only build if necessary
+build_nav = False
+
 # Convert
 nav_entries = []
 for markdown_file in markdown_files:
     adoc_file = markdown_file.replace('.md', '.adoc')
     print("Converting " + markdown_file + "...")
-    # subprocess.check_output(kramdoc_path + ' --format=GFM' + ' --output=' + adoc_file + ' --wrap=ventilate ' + markdown_file)
-    nav_entries.append(adoc_file)
+    subprocess.check_output(kramdoc_path + ' --format=GFM' + ' --output=' + adoc_file + ' --wrap=ventilate ' + markdown_file)
+    if build_nav:
+        nav_entries.append(adoc_file)
 
-# Build navigation
-last_folder = ''
-nav_file = open("modules/ROOT/nav.adoc", "w")
-for nav_entry in nav_entries:
-    link_file = nav_entry.replace("\\", "/")
-    link_name = link_file
-    link_name = os.path.basename(os.path.normpath(os.path.dirname(link_file)))
-    link_name = link_name.replace('_', ' ')
-    link_file = link_file.replace(dir_path + '/', '')
-    split_str = link_file.split('/')
-    # if (last_folder != split_str[0]):
-    #     nav_file.write("*")
-    if len(split_str) > 2:
-        if split_str[0] != last_folder:
-            last_folder = split_str[0]
-            # nav_file.write("* xref:%s[%s]\n" % (last_folder, 'README.md'))
-    nav_file.write("* xref:%s[%s]\n" % (link_file, link_name))
-nav_file.closed
+if build_nav:
+    # Build navigation
+    last_folder = ''
+    nav_file = open("modules/ROOT/nav.adoc", "w")
+    for nav_entry in nav_entries:
+        link_file = nav_entry.replace("\\", "/")
+        link_name = link_file
+        link_name = os.path.basename(os.path.normpath(os.path.dirname(link_file)))
+        link_name = link_name.replace('_', ' ')
+        link_file = link_file.replace(dir_path + '/', '')
+        split_str = link_file.split('/')
+        # if (last_folder != split_str[0]):
+        #     nav_file.write("*")
+        if len(split_str) > 2:
+            if split_str[0] != last_folder:
+                last_folder = split_str[0]
+                # nav_file.write("* xref:%s[%s]\n" % (last_folder, 'README.md'))
+        nav_file.write("* xref:%s[%s]\n" % (link_file, link_name))
+    nav_file.closed
