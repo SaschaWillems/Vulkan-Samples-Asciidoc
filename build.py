cceff06769caacc7ced3a0442a5735cd4a537c77
Sascha Willems <webmaster@saschawillems.de>
2023-08-01 22:16:03 +0200

    Work-in-progess script to build antora structur from samples repo structure

--- build.py
+++ build.py
@@ -17,7 +17,7 @@ def convert_folder(folder_name):
     src_folder = SAMPLES_ROOT + "/" + folder_name
     os.makedirs(dst_folder, 0o777, True)
     for root, dirs, files in os.walk(src_folder):
-        for file in files:#
+        for file in files:
             # Docs
             if (file.endswith(".adoc")):                
                 dst_sub_folder = dst_folder + "/" + os.path.basename(root)
@@ -28,15 +28,15 @@ def convert_folder(folder_name):
                     shutil.copy(os.path.join(root, file), dst_sub_folder + "/" + file)
                     # We need to add the image folder to the doc to match how Antora expects images to be stored
                     with open(dst_sub_folder + "/" + file, "r+") as f:
-                        dst_image_sub_folder = dst_image_folder + "/" + os.path.basename(root)
                         s = f.read()
                         f.seek(0)
-                        f.write(":images: " + folder_name + "/" + os.path.basename(root) + "\n" + s)
+                        f.write(":images: " + root.replace("../samples/", "") + "\n" + s)
                         f.close()
             # Antora requires images to be in a predefined folder, which clashes with how images are stored in the repo (besides the docs)                    
-            if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
-                dst_image_sub_folder = dst_image_folder + "/" + os.path.basename(root)
-                # @todo: only if images exist for this sample
+            if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):                
+                dst_image_sub_folder = ANTORA_ROOT + "/images/" + root.replace("../samples/", "")
+                # print(os.path.join(root, file))
+                # print(root.replace("../samples/", ""))
                 os.makedirs(dst_image_sub_folder, 0o777, True)
                 shutil.copy(os.path.join(root, file), dst_image_sub_folder + "/" + file)
  
