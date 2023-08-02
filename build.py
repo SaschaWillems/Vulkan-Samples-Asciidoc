0b25cff08d77a927c8dd7aee931ef1ed8061e0f8
Sascha Willems <webmaster@saschawillems.de>
2023-08-02 18:27:24 +0200

    Adjust image path
    Add link to repo code

--- build.py
+++ build.py
@@ -26,17 +26,29 @@ def convert_folder(folder_name):
                 else:
                     os.makedirs(dst_sub_folder, 0o777, True)
                     shutil.copy(os.path.join(root, file), dst_sub_folder + "/" + file)
-                    # We need to add the image folder to the doc to match how Antora expects images to be stored
+                    # We want/need to add in some additional stuff into the readme during the build process
                     with open(dst_sub_folder + "/" + file, "r+") as f:
                         s = f.read()
+                        # We need to add the image folder to the doc to match how Antora expects images to be stored
+                        added_text = ":images: " + root.replace("../samples/", "") + "\n"
+                        # Add reference to sample in github repo so people can easily find the sample code
+                        repo_node_added = False
+                        repo_sample_url = "https://github.com/KhronosGroup/Vulkan-Samples/tree/main/samples/" + root.replace("../samples/", "")
+                        repo_note = "\n\nTIP: The source for this sample can be found in the " + repo_sample_url + "[Khronos Vulkan samples github repository].\n" 
+                        first_caption_pos = s.find("=")
+                        if first_caption_pos > -1:
+                            end_line_pos = s.find("\n", first_caption_pos)
+                            if end_line_pos > -1: 
+                                s = s[:end_line_pos] + repo_note + s[end_line_pos:]
+                                repo_node_added = True
+                        if not repo_node_added:
+                            print('Warning: Could not add repo link in ' + file)
                         f.seek(0)
-                        f.write(":images: " + root.replace("../samples/", "") + "\n" + s)
+                        f.write(added_text + s)
                         f.close()
             # Antora requires images to be in a predefined folder, which clashes with how images are stored in the repo (besides the docs)                    
             if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):                
                 dst_image_sub_folder = ANTORA_ROOT + "/images/" + root.replace("../samples/", "")
-                # print(os.path.join(root, file))
-                # print(root.replace("../samples/", ""))
                 os.makedirs(dst_image_sub_folder, 0o777, True)
                 shutil.copy(os.path.join(root, file), dst_image_sub_folder + "/" + file)
  
